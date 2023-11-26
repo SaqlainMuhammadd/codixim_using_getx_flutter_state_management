67c004e8484d3b7bde5970e7e7a051b210c338c0
Muhammad Saqlain <miansaqlainqureshi@gmail.com>
2023-11-26 09:32:01 -0800

    updated and created new screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,30 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'CodiXim',
       theme: ThemeData(
-          scaffoldBackgroundColor: Colors.white,
-          useMaterial3: true,
-          textTheme: TextTheme(displayLarge: TextStyle())),
+        scaffoldBackgroundColor: const Color.fromARGB(
+            0, 255, 0, 0), // Make scaffold background transparent
+        canvasColor: const Color.fromARGB(
+            0, 202, 19, 19), // Make canvas color transparent
+        useMaterial3: true,
+        textTheme: TextTheme(displayLarge: TextStyle()),
+      ),
       home: AnimatedSplashScreen(
-        splash: Image.asset('assets/images/codiximlogo.png'),
+        splash: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.blue,
+                Colors.green,
+                // Add more colors as needed
+              ],
+            ),
+          ),
+          // child: Center(
+          //   child: Image.asset('assets/images/codiximlogo.png'),
+          // ),
+        ),
         nextScreen: HomeScreen(),
         splashTransition: SplashTransition.fadeTransition,
         duration: 3000,
