93d17479a62635ce4b9a0b450cef7d4a6ea6aaad
Muhammad Saqlain <miansaqlainqureshi@gmail.com>
2023-12-09 18:18:04 -0800

    up

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
